54a5984731ba878e8f4b22a92c63d264b7560d05
ssnwmman <ssnwmman@gmail.com>
2025-05-25 15:34:59 +0900

    second commit

--- frontend/frontend/src/pages/Home.tsx
+++ frontend/frontend/src/pages/Home.tsx
@@ -10,13 +10,16 @@ const Home: React.FC = () => {
     const handleGoToTest = () => {
     navigate('/Test');
     };
+    const handleGoToUm = () => {
+      navigate('/');
+    };
 
   return (
     <div className='container'>
       <h1>IT 초성 퀴즈</h1>
       <button onClick={handleGoToAbout}>About페이지로 이동</button>
       <button onClick={handleGoToTest}>Test페이지로 이동</button>
-      <button onClick={handleGoToTest}>아직 미정</button>
+      <button onClick={handleGoToUm}>아직 미정</button>
     </div>
   );
 };
