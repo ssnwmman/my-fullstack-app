a93cf69d77349cf9b77f280f9268390323e77f01
ssnwmman <ssnwmman@gmail.com>
2025-06-07 11:37:23 +0900

    add API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
+import requests
 import random
 
 app = FastAPI()
@@ -12,11 +13,72 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-questions = [
-    {"question": "ㄱㅅ", "answer": "감사", "hint": "고마울 때 쓰는 말"},
-    {"question": "ㅎㅅ", "answer": "학생", "hint": "학교 다니는 사람"},
-    {"question": "ㅅㅌ", "answer": "사탕", "hint": "달콤한 간식"},
-]
+
+API_URL = "https://opendict.korean.go.kr/api/search"
+API_KEY = "AB2823EF55183E6C1EC3CE18828C8514"
+MAX_NUM = 100
+
+def get_initials(word: str) -> str:
+    CHOSUNG_LIST = [
+        "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ",
+        "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ",
+        "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"
+    ]
+    HANGUL_BASE = 0xAC00
+    initials = []
+    for char in word:
+        code = ord(char)
+        if 0xAC00 <= code <= 0xD7A3:
+            code -= HANGUL_BASE
+            chosung_index = code // 588
+            initials.append(CHOSUNG_LIST[chosung_index])
+        else:
+            initials.append(char)
+    return "".join(initials)
+
+count = 100
+
+params = {
+        "key": API_KEY,
+        "target": "domain",
+        "q": "정보통신",
+        "type1": "word",
+        "req_type": "json",
+        "num": MAX_NUM,
+        "start": 1
+    }
+
+response = requests.get(API_URL, params=params)
+if response.status_code != 200:
+        {
+        "error": "API 요청 실패",
+        "status_code": response.status_code,
+        "response_text": response.text
+        }
+
+
+data = response.json()
+items = data.get("channel", {}).get("item", [])
+
+questions = []
+for item in items:
+    raw_word = item.get("word", "")
+    parts = raw_word.split("^")
+    filtered_parts = [p for p in parts if p not in ("정보", "통신")]
+    cleaned_word = "".join(filtered_parts).strip()
+
+    senses = item.get("sense", [])
+    if senses and isinstance(senses, list):
+            definition = senses[0].get("definition", "뜻풀이가 없습니다.")
+    else:
+            definition = "뜻풀이가 없습니다."
+
+    if cleaned_word:
+        answer = cleaned_word
+        initial_word = get_initials(cleaned_word)
+        questions.append({"question":initial_word,
+                            "answer":answer,
+                            "hint":definition})
 
 class ConnectionManager:
     def __init__(self):
